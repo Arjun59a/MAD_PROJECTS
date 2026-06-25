9ba2547c2d5c55e075fa1f776147da0583de4ee1
Arjun_59a <at3903334@gmail.com>
2026-06-25 14:23:23 +0530

    Intial Coimmit

---
+++ src/p1_2.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+    val i = 30000
+    val j = i.toDouble()
+    val str = "10"
+    val k = str.toInt()
+    val str1 = "400000.5699"
+    val db = str1.toDouble()
+    var i1 = str1.toInt()
+
+    println(" i = $i")
+    println("j = $j")
+    println("k = $k")
+    println("str = $str")
+    println("DB = $db")
+    println("i1  = $i1")
+
+}

35bb890f5a160fd8303ddfbb8279a5cfca035dcd
Arjun_59a <at3903334@gmail.com>
2026-06-25 14:06:53 +0530

    Intial Coimmit

---
+++ src/p1_1.kt
@@ -0,0 +1,28 @@
+
+fun main()
+{
+
+    var j : Int = 100
+    var k : Float = 55F
+    var m : Double = 100.00
+    var ch : Char = 'a'
+    var str : String = "This is me Arjun Thakor"
+    var bl : Boolean = false
+    var bt : Byte = 8
+    var sht : Short = 32
+    val z  = 20000
+
+
+
+    println("Integer Variable j = "+j)
+    println("Float Variabke k ="+k)
+    println("Douoble Variable m = "+m)
+    println("Char Variable ch = "+ch)
+    println("String Variable str = "+str)
+    println("This is the boolean Variable bl = "+bl)
+    println("This is the Byte variable bt = "+bt)
+    println("This is the short variable sht = "+sht)
+    println("This is the variable z = "+z)
+
+
+}
